04d59de5dde693b95b78e4141fc8dc2ac7e391f8
basher <basher@madasafish.com>
2025-01-31 11:40:03 +0000

    modify build process - test local publish storybook

---
+++ UI/public/build/javascript/polyfills.817977f3.js
@@ -0,0 +1,2 @@
+(0,("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire94c2.register)("fZQ7f",function(e,f){});
+//# sourceMappingURL=polyfills.817977f3.js.map
