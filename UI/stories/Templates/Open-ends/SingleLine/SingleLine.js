74975854f0e0fc68ba36dd341b8d45ff792406f3
basher <basher@madasafish.com>
2025-02-04 15:09:12 +0000

    simplify singlelineedit with special codes template

--- UI/stories/Templates/Open-ends/SingleLine/SingleLine.js
+++ UI/stories/Templates/Open-ends/SingleLine/SingleLine.js
@@ -52,16 +52,11 @@ export const SingleLineSpecialCodesHtml = () => `
             data-questionid="_Q0"
             data-questiongroup="_QText"
         >
-            <m-input-singlelineedit
-                data-questionid="_Q0"
-                data-questiongroup="_QText"
-            >
-                <input
-                    type="text"
-                    id="_Q0"
-                    class="a-input-singlelineedit"
-                />
-            </m-input-singlelineedit>
+            <input
+                type="text"
+                id="_Q0"
+                class="a-input-singlelineedit"
+            />
 
             <div class="m-option-wrapper">
                 <div class="m-option-base m-option-single-answer">
