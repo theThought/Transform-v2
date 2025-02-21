554d6b092da7ab5540a37cb3a2e36430112fc292
basher <basher@madasafish.com>
2025-02-21 10:56:01 +0000

    add missing l-row to example template

--- UI/stories/Templates/1. Example/Example.js
+++ UI/stories/Templates/1. Example/Example.js
@@ -32,13 +32,17 @@ ${htmlFragmentCustomProperties}
                 </div>
             </div>
 
-            <div class="m-message-information">
-                <div>
-                    <ul>
-                        <li>This is a simple example to demonstrate the structure of a question template.</li>
-                        <li>It also demonstrates how the custom properties can be modified via Storybook controls, enabling ZeroHeight users to generate the JSON they need when building survey scripts.</li>
-                        <li>Edit the "<code>ExampleCustomProp</code>" text string, then click the "<code>Generate &amp; copy JSON</code>" button above.</li>
-                    </ul>
+            <div class="l-row">
+                <div class="l-column">
+                    <div class="m-message-information">
+                        <div>
+                            <ul>
+                                <li>This is a simple example to demonstrate the structure of a question template.</li>
+                                <li>It also demonstrates how the custom properties can be modified via Storybook controls, enabling ZeroHeight users to generate the JSON they need when building survey scripts.</li>
+                                <li>Edit the "<code>ExampleCustomProp</code>" text string, then click the "<code>Generate &amp; copy JSON</code>" button above.</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </o-question>
