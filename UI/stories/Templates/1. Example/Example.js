641872d0b8140730b3796c28021e4bd38afd8a43
basher <basher@madasafish.com>
2025-02-21 10:24:12 +0000

    fix template example notes

--- UI/stories/Templates/1. Example/Example.js
+++ UI/stories/Templates/1. Example/Example.js
@@ -35,8 +35,7 @@ ${htmlFragmentCustomProperties}
             <div class="m-message-information">
                 <div>
                     <ul>
-                        <li>This is a simple example to demonstrate the structure of a question.</li>
-                        <li><b>HTML markup still TBC</b>. For example, what is the correct DOM order for <code>&lt;o-question&gt;</code> and <code>&lt;o-question-response&gt;</code>?</li>
+                        <li>This is a simple example to demonstrate the structure of a question template.</li>
                         <li>It also demonstrates how the custom properties can be modified via Storybook controls, enabling ZeroHeight users to generate the JSON they need when building survey scripts.</li>
                         <li>Edit the "<code>ExampleCustomProp</code>" text string, then click the "<code>Generate &amp; copy JSON</code>" button above.</li>
                     </ul>
