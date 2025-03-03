964404af5ad78f0c47ae3390b3905bbd71cebf20
basher <basher@madasafish.com>
2025-03-03 10:07:34 +0000

    fix css grid for storybook example

---
+++ UI/_example-pages/build/javascript/polyfills.b3d28177.js
@@ -0,0 +1,2 @@
+(0,("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire4957.register)("cy3Db",function(e,n){});
+//# sourceMappingURL=polyfills.b3d28177.js.map
--- UI/stories/Pages/Page.js
+++ UI/stories/Pages/Page.js
@@ -1,36 +1,55 @@
+const style = `
+<style>
+    /* Mimic CSS Grid behaviour of <body> in Survey pages. */
+    #storybook-root {
+        display: grid;
+        gap: var(--space-4);
+        grid-template-areas:
+            '. header .'
+            '. main .'
+            '. footer .';
+        grid-template-columns:
+            1fr minmax(var(--page-min-width), var(--page-max-width))
+            1fr;
+        grid-template-rows: var(--header-min-height) 1fr var(--footer-min-height);
+        min-block-size: 100vh;
+    }
+</style>
+`;
+
 export const PageHtml = () => `
-<div class="page">
-    <header class="header">
-        <p>Page header content goes inside <code>&lt;header&gt;</code>.</p>
-    </header>
+${style}
 
-    <main class="surroundcontent">
-        <form action="#">
+<header class="header">
+    <p>Page header content goes inside <code>&lt;header&gt;</code>.</p>
+</header>
 
-            <o-question class="l-stack">
-                <div class="l-cover"><!-- cover --></div>
+<main class="surroundcontent">
+    <form action="#">
 
-                <div class="l-row">
-                    <div class="l-column">
-                        <div>QUESTION GOES HERE...</div>
-                    </div>
+        <o-question class="l-stack">
+            <div class="l-cover"><!-- cover --></div>
 
-                    <div class="l-column">
-                        <o-response>
-                            <div>
-                                FORM CONTROL(S) GO HERE...
-                            </div>
-                        </o-response>
-                    </div>
+            <div class="l-row">
+                <div class="l-column">
+                    <div>QUESTION GOES HERE...</div>
                 </div>
 
-            </o-question>
+                <div class="l-column">
+                    <o-response>
+                        <div>
+                            FORM CONTROL(S) GO HERE...
+                        </div>
+                    </o-response>
+                </div>
+            </div>
 
-        </form>
-    </main>
+        </o-question>
 
-    <footer class="footer">
-        <p>Page footer content goes inside <code>&lt;footer&gt;</code>.</p>
-    </footer>
-</div>
+    </form>
+</main>
+
+<footer class="footer">
+    <p>Page footer content goes inside <code>&lt;footer&gt;</code>.</p>
+</footer>
 `;
