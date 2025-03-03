4327a3c4d246db829740f4dad5553f56b5ece2f2
basher <basher@madasafish.com>
2025-03-03 11:22:03 +0000

    remove build folder from git

--- UI/public/build/javascript/polyfills.b3d28177.js
+++
@@ -1,2 +0,0 @@
-(0,("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire4957.register)("cy3Db",function(e,n){});
-//# sourceMappingURL=polyfills.b3d28177.js.map
