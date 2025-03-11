08f44d07e57d5c2234f927c16cdfc3378f24a2c7
basher <basher@madasafish.com>
2025-03-11 15:15:38 +0000

    fix o-option-sublist html/css

--- UI/stories/Organisms/Sublist/Sublist.js
+++ UI/stories/Organisms/Sublist/Sublist.js
@@ -1,37 +1,36 @@
 export const SublistHtml = () => `
-<fieldset
-    class="o-option-sublist"
-    aria-describedby="question-id"
->
-    <legend class="a-label-heading-sublist">
-        Sublist heading is a <code>&lt;legend&gt;</code>
-    </legend>
-    <m-option-base class="m-option-single-answer">
-        <input type="radio" id="radio1" name="radios" />
-        <label for="radio1">
-            <span class="a-icon-multistate" data-icon-type="single"></span>
-            <span class="a-label-option">
-                Radio 1 label
-            </span>
-        </label>
-    </m-option-base>
-    <m-option-base class="m-option-single-answer">
-        <input type="radio" id="radio2" name="radios" />
-        <label for="radio2">
-            <span class="a-icon-multistate" data-icon-type="single"></span>
-            <span class="a-label-option">
-                Radio 2 label
-            </span>
-        </label>
-    </m-option-base>
-    <m-option-base class="m-option-single-answer">
-        <input type="radio" id="radio3" name="radios" />
-        <label for="radio3">
-            <span class="a-icon-multistate" data-icon-type="single"></span>
-            <span class="a-label-option">
-                Radio 3 label
-            </span>
-        </label>
-    </m-option-base>
-</fieldset>
+<o-option-sublist>
+    <fieldset aria-describedby="question-id">
+        <legend class="a-label-heading-sublist">
+            Sublist heading is a <code>&lt;legend&gt;</code>
+        </legend>
+        <m-option-base class="m-option-single-answer">
+            <input type="radio" id="radio1" name="radios" />
+            <label for="radio1">
+                <span class="a-icon-multistate" data-icon-type="single"></span>
+                <span class="a-label-option">
+                    Radio 1 label
+                </span>
+            </label>
+        </m-option-base>
+        <m-option-base class="m-option-single-answer">
+            <input type="radio" id="radio2" name="radios" />
+            <label for="radio2">
+                <span class="a-icon-multistate" data-icon-type="single"></span>
+                <span class="a-label-option">
+                    Radio 2 label
+                </span>
+            </label>
+        </m-option-base>
+        <m-option-base class="m-option-single-answer">
+            <input type="radio" id="radio3" name="radios" />
+            <label for="radio3">
+                <span class="a-icon-multistate" data-icon-type="single"></span>
+                <span class="a-label-option">
+                    Radio 3 label
+                </span>
+            </label>
+        </m-option-base>
+    </fieldset>
+</o-option-sublist>
 `;
--- UI/stories/Templates/Open-ends/SingleLine/SingleLine.js
+++ UI/stories/Templates/Open-ends/SingleLine/SingleLine.js
@@ -81,29 +81,28 @@ export const SingleLineSpecialCodesHtml = () => `
                             />
                         </m-input-singleline>
 
-                        <fieldset
-                            class="o-option-sublist"
-                            aria-describedby="question-id"
-                        >
-                            <m-option-base class="m-option-single-answer" data-exclusive="true">
-                                <input type="radio" id="radio1" name="radios" />
-                                <label for="radio1">
-                                    <span class="a-icon-multistate" data-icon-type="single"></span>
-                                    <span class="a-label-option">
-                                        Special code 1
-                                    </span>
-                                </label>
-                            </m-option-base>
-                            <m-option-base class="m-option-single-answer" data-exclusive="true">
-                                <input type="radio" id="radio2" name="radios" />
-                                <label for="radio2">
-                                    <span class="a-icon-multistate" data-icon-type="single"></span>
-                                    <span class="a-label-option">
-                                        Special code 2
-                                    </span>
-                                </label>
-                            </m-option-base>
-                        </fieldset>
+                        <o-option-sublist>
+                            <fieldset aria-describedby="question-id">
+                                <m-option-base class="m-option-single-answer" data-exclusive="true">
+                                    <input type="radio" id="radio1" name="radios" />
+                                    <label for="radio1">
+                                        <span class="a-icon-multistate" data-icon-type="single"></span>
+                                        <span class="a-label-option">
+                                            Special code 1
+                                        </span>
+                                    </label>
+                                </m-option-base>
+                                <m-option-base class="m-option-single-answer" data-exclusive="true">
+                                    <input type="radio" id="radio2" name="radios" />
+                                    <label for="radio2">
+                                        <span class="a-icon-multistate" data-icon-type="single"></span>
+                                        <span class="a-label-option">
+                                            Special code 2
+                                        </span>
+                                    </label>
+                                </m-option-base>
+                            </fieldset>
+                        </o-option-sublist>
                     </o-response>
                 </div>
             </div>
