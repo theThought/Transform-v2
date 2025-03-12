ed7dd60a5328d5be430f2cd934cc3902edfe6ffa
Richard Abrams <richabramsuk@outlook.com>
2025-03-12 09:46:26 +0000

    Revise the order of component initialisation.
    Adds comments clarifying the importance of the sequence in which the client initialises components.

--- UI/src/javascript/ui-init.ts
+++ UI/src/javascript/ui-init.ts
@@ -1,7 +1,7 @@
 // DEMO web component for Storybook introduction.
 import WCExample from './web-components/wc-example';
 
-// Survey web components.
+// Import modules for web components.
 import MInputSingleline from './web-components/m-input-singleline';
 import MInputSinglelineNumber from './web-components/m-input-singleline-number';
 import MOptionBase from './web-components/m-option-base';
@@ -15,9 +15,19 @@ export const uiInit = (): void => {
     !customElements.get('wc-example') &&
         customElements.define('wc-example', WCExample);
 
-    // Survey web components.
+    // Define custom elements in the client.
+    // The order in which these elements are listed below is the order in which
+    // the client will initialise the elements. It is therefore important to
+    // consider the sequence of define calls: i.e. inputs belong to a response
+    // which belongs to a question and may require the question to be present.
+
+    // Outer containers.
     !customElements.get('o-question') &&
         customElements.define('o-question', OQuestion);
+    !customElements.get('o-response') &&
+        customElements.define('o-response', OResponse);
+
+    // Inner components.
     !customElements.get('m-input-singleline') &&
         customElements.define('m-input-singleline', MInputSingleline);
     !customElements.get('m-input-singleline-number') &&
@@ -31,6 +41,4 @@ export const uiInit = (): void => {
         customElements.define('o-combobox', OCombobox);
     !customElements.get('o-dropdown') &&
         customElements.define('o-dropdown', ODropdown);
-    !customElements.get('o-response') &&
-        customElements.define('o-response', OResponse);
 };
