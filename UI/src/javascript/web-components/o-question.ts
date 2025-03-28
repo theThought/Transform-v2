bb6df1b30586288a9f6a9ca0556c93deb2b10d8d
Richard Abrams <richabramsuk@outlook.com>
2025-03-28 13:03:08 +0000

    Improve collapsed/mobile layout
    Removes whitespace from rows in order to make the :empty pseudo-selector effective.
    
    The gap property was causing unwanted vertical space on collapsed side-by-side layouts. This is mitigated by adding an :empty class to hide completely empty rows and removing the whitespace.

--- UI/src/javascript/web-components/o-question.ts
+++ UI/src/javascript/web-components/o-question.ts
@@ -1,5 +1,26 @@
+import { removeHTMLWhitespace } from './util';
+
 export default class OQuestion extends HTMLElement {
     constructor() {
         super();
+
+        this.init();
+    }
+
+    private init(): void {
+        this.cleanEmptyLayout();
+    }
+
+    private cleanEmptyLayout(): void {
+        const layoutItems = this.querySelectorAll('.l-row');
+
+        if (!layoutItems) {
+            return;
+        }
+
+        layoutItems.forEach(
+            (element) =>
+                (element.innerHTML = removeHTMLWhitespace(element.innerHTML)),
+        );
     }
 }
