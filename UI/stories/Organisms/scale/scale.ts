a4ebb30b2c0f3fb13b2fa2d7f2ac9ca8239689a7
CreativeStrategyGuy <kevin.gray@ipsos.com>
2025-04-03 10:29:11 +0100

    injecting prepost properties into scale structures

--- UI/stories/Organisms/scale/scale.ts
+++ UI/stories/Organisms/scale/scale.ts
@@ -1,9 +1,33 @@
-export const ScaleHtml = (): string => `
-<o-scale>
+export const ScaleHtml = (args): string => `
+<o-scale 
+    data-question-id="_Q0"
+    data-question-group="_QText"
+    data-properties='{
+        "labels":{
+            "pre":"${args.PreLabel}",
+            "post":"${args.PostLabel}"
+        }
+    }'>
+    <div class="m-label-prepost">
+        <span class="a-label-pre">${args.PreLabel}</span>
+        <span class="a-label-post">${args.PostLabel}</span>
+    </div>
+    <o-scale-container>
 </o-scale>
 `;
 
 export const ScaleVerticalHtml = (): string => `
-<o-scale-vertical>
+<o-scale-vertical 
+    data-question-id="_Q1"
+    data-question-group="_QText"
+    data-properties='{
+        "labels":{
+            "pre":"${args.PreLabel}",
+            "post":"${args.PostLabel}"
+        }
+    }'>
+    <span class="a-label-pre">${args.PreLabel}</span>
+    <o-scale-container />
+    <span class="a-label-post">${args.PostLabel}</span>
 </o-scale-vertical>
 `;
