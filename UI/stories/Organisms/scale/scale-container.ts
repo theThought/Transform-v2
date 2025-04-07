fa4d3c4295c97c4c67c3c6f1e0d8e0c1aa5e7099
CreativeStrategyGuy <kevin.gray@ipsos.com>
2025-04-07 11:43:15 +0100

    Storybook: attempted to embed m-scale-unit inside scalecontainer

--- UI/stories/Organisms/scale/scale-container.ts
+++ UI/stories/Organisms/scale/scale-container.ts
@@ -1,5 +1,17 @@
+
+import MScaleUnit from '../../Molecules/scale/scale-unit.stories';
+
 export const ScaleContainerHtml = (): string => `
 <o-scale-container>
-    <m-scale-unit data-value="1" />
+    $(MScaleUnit.ScaleUnitHtml({ dataValue: 1 }))
+    $(MScaleUnit.ScaleUnitHtml({ dataValue: 2 }))
+    $(MScaleUnit.ScaleUnitHtml({ dataValue: 3 }))
+    $(MScaleUnit.ScaleUnitHtml({ dataValue: 4 }))
+    $(MScaleUnit.ScaleUnitHtml({ dataValue: 5 }))
+    $(MScaleUnit.ScaleUnitHtml({ dataValue: 6 }))
+    $(MScaleUnit.ScaleUnitHtml({ dataValue: 7 }))
+    $(MScaleUnit.ScaleUnitHtml({ dataValue: 8 }))
+    $(MScaleUnit.ScaleUnitHtml({ dataValue: 9 }))
+    $(MScaleUnit.ScaleUnitHtml({ dataValue: 10 }))
 </o-scale-container>
 `;
