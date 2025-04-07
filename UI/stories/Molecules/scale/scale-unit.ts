1fd8ef7dc76fb10d5a59e56884bc7f5de9c814c0
CreativeStrategyGuy <kevin.gray@ipsos.com>
2025-04-07 10:36:30 +0100

    Storybook: forgot to define args again

--- UI/stories/Molecules/scale/scale-unit.ts
+++ UI/stories/Molecules/scale/scale-unit.ts
@@ -1,4 +1,4 @@
-export const ScaleUnitHtml = (): string => `
+export const ScaleUnitHtml = (args): string => `
 <m-scale-unit data-value="1" style="background-position-x: 0;">
     <span class="a-label" data-value="${args.dataValue}">${args.dataValue}</span>
 </m-scale-unit>
