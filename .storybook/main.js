42e3c1f5fafcd855dae9de8c9c914e32a8a37d40
CreativeStrategyGuy <kevin.gray@ipsos.com>
2025-04-17 00:11:34 +0100

    Storybook: some configuration file changes to find the right root path

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,11 @@
 const path = require('path');
 
 module.exports = {
-    staticDirs: ['../stories/Dimensions'],
-    // ...existing code...
+    stories: ['../UI/stories/**/*.stories.@(js|jsx|ts|tsx)'], // Include your stories
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+    ],
+    staticDirs: ['../UI/stories/Dimensions'], // Correct path to the Dimensions folder
+    framework: '@storybook/web-components-vite', // Specify the framework
 };
