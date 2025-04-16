fee3f42aab468aa4f93dcb0a8e7bc71f5b9a0691
CreativeStrategyGuy <kevin.gray@ipsos.com>
2025-04-16 18:27:29 +0100

    Storybook: getting rid of static file location definition

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,5 +2,4 @@ const path = require('path');
 
 module.exports = {
     // ...existing code...
-    staticDirs: [path.resolve(__dirname, '../UI/stories/Templates')], // Serve the Templates directory as static
 };
