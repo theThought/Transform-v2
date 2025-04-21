429eed3aebb7c239978745ebb69fc3f1e55448c3
CreativeStrategyGuy <kevin.gray@ipsos.com>
2025-04-21 15:36:02 +0100

    Storybook: setting input parameters based on conrols

--- UI/stories/Atoms/Inputs/Input.ts
+++ UI/stories/Atoms/Inputs/Input.ts
@@ -3,6 +3,30 @@ export function ASingleline(args:any): HTMLInputElement {
     const container: HTMLInputElement = document.createElement('input');
     container.setAttribute('type', 'text');
     container.classList.add('a-singleine');
+    switch (args.type) {
+        case 'text':
+            container.setAttribute('type', 'text');
+            container.setAttribute('maxlength', args.maximum);
+            break;
+        case 'number':
+            container.setAttribute('type', 'number');
+            container.setAttribute('min', args.minimum);
+            container.setAttribute('max', args.maximum);
+            container.setAttribute('step', '1');
+            break;
+        case "date":
+            container.setAttribute('type', 'date');
+            container.setAttribute('min', args.minimum);
+            container.setAttribute('max', args.maximum);
+            break;
+        case "range":
+            container.setAttribute('type', 'range');
+            container.setAttribute('min', args.minimum);
+            container.setAttribute('max', args.maximum);
+            break
+    }
+
+    container.setAttribute('min', args.minimum);
     return container;
 }
 
