7c8c90123710fb3cd2bbf66df06d83eb27819373
Richard Abrams <richabramsuk@outlook.com>
2025-05-01 01:12:48 +0100

    Simple vertical implementation

--- UI/stories/Molecules/scale/scale-container.ts
+++ UI/stories/Molecules/scale/scale-container.ts
@@ -1,19 +1,24 @@
-import MScaleContainer from "../../../src/javascript/web-components/m-scale-container";
-import AScaleUnit from "../../../src/javascript/web-components/a-scale-unit";
+import MScaleContainer from '../../../src/javascript/web-components/m-scale-container';
+import AScaleUnit from '../../../src/javascript/web-components/a-scale-unit';
 
 export function MScaleContainer_Story(args: any): HTMLElement {
-    
-    const container: MScaleContainer = document.createElement('m-scale-container');
-    
-    for (let counter: number = args.minimum; counter <= args.maximum; counter++) {
-        const scaleUnit: AScaleUnit = document.createElement('a-scale-unit') as AScaleUnit;
+    const container: MScaleContainer =
+        document.createElement('m-scale-container');
+
+    for (
+        let counter: number = args.minimum;
+        counter <= args.maximum;
+        counter++
+    ) {
+        const scaleUnit: AScaleUnit = document.createElement(
+            'a-scale-unit',
+        ) as AScaleUnit;
         scaleUnit.dataValue = counter.toString(); // Set dataValue as string
         container.appendChild(scaleUnit);
 
-        container.setAttribute(
-            'style',
-            `width: ${args.width};`
-        );
+        container.setAttribute('style', `width: ${args.width};`);
+
+        container.classList.add(`orientation-${args.orientation}`);
     }
 
     return container;
