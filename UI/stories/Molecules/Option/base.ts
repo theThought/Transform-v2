3f20323de8d5216182f217df4a769005b13b2bfc
CreativeStrategyGuy <kevin.gray@ipsos.com>
2025-05-07 14:13:00 +0100

    Storybook: simpler MOptionBase

--- UI/stories/Molecules/Option/base.ts
+++ UI/stories/Molecules/Option/base.ts
@@ -2,70 +2,37 @@
 
 export function MOptionBase_Story(args: any): HTMLElement {
     const container: HTMLElement = document.createElement('m-option-base');
+    const inputType: string = args.exclusive ? 'radio' : 'checkbox';
+    const containerClass: string = args.exclusive
+        ? 'm-option-single-answer'
+        : 'm-option-multiple-answer';
 
-    if (args.exclusive || args.iconType === 'radio') {
-        container.setAttribute('data-exclusive', args.exclusive);
-        container.setAttribute('data-question-id', args.questionId);
-        container.setAttribute('data-question-group', args.questionName);
-        container.setAttribute('class', 'below');
-        container.setAttribute('data-hidden', 'false');
+    container.setAttribute('data-exclusive', args.exclusive);
+    container.setAttribute('data-question-id', args.questionId);
+    container.setAttribute('data-question-group', args.questionName);
+    container.setAttribute('class', containerClass + ' below');
+    container.setAttribute('data-hidden', 'false');
 
-        const inputElement: HTMLInputElement = document.createElement('input');
-        inputElement.setAttribute('type', 'radio');
-        inputElement.setAttribute('id', args.questionId + args.categoryId);
-        inputElement.setAttribute('name', args.questionName);
-        inputElement.setAttribute('data-question-group', args.questionName);
-        container.appendChild(inputElement);
+    const inputElement: HTMLInputElement = document.createElement('input');
+    inputElement.setAttribute('type', inputType);
+    inputElement.setAttribute('id', args.questionId + args.categoryId);
+    inputElement.setAttribute('name', args.questionName);
+    inputElement.setAttribute('data-question-group', args.questionName);
+    container.appendChild(inputElement);
 
-        const labelElement = document.createElement('label');
-        labelElement.setAttribute('for', args.questionId + args.categoryId);
+    const labelElement = document.createElement('label');
+    labelElement.setAttribute('for', args.questionId + args.categoryId);
 
-        const iconElement = document.createElement('span');
-        iconElement.setAttribute('class', 'a-icon-multistate');
-        iconElement.setAttribute('data-icon-type', args.iconType);
-        labelElement.appendChild(iconElement);
+    const iconElement = document.createElement('span');
+    iconElement.setAttribute('class', 'a-icon-multistate');
+    iconElement.setAttribute('data-icon-type', args.iconType);
+    labelElement.appendChild(iconElement);
 
-        const textElement = document.createElement('span');
-        textElement.setAttribute('class', 'a-label-option');
-        textElement.textContent = args.optionLabel;
-        labelElement.appendChild(textElement);
-        container.appendChild(labelElement);
-    }
+    const textElement = document.createElement('span');
+    textElement.setAttribute('class', 'a-label-option');
+    textElement.textContent = args.optionLabel;
+    labelElement.appendChild(textElement);
+    container.appendChild(labelElement);
 
-    if (!args.exclusive || args.iconType === 'check') {
-        container.setAttribute('class', 'm-option-multiple-answer');
-
-        const inputElement = document.createElement('input');
-        inputElement.setAttribute('type', 'checkbox');
-        inputElement.setAttribute('id', args.questionId + args.categoryId);
-        inputElement.setAttribute('name', args.questionName);
-        inputElement.setAttribute('data-question-group', args.questionName);
-        container.appendChild(inputElement);
-
-        const labelElement = document.createElement('label');
-        labelElement.setAttribute('for', args.questionId + args.categoryId);
-
-        const iconElement = document.createElement('span');
-        iconElement.setAttribute('class', 'a-icon-multistate');
-        iconElement.setAttribute('data-icon-type', args.iconType);
-        labelElement.appendChild(iconElement);
-
-        const textElement = document.createElement('span');
-        textElement.setAttribute('class', 'a-label-option');
-        textElement.textContent = args.optionLabel;
-        labelElement.appendChild(textElement);
-        container.appendChild(labelElement);
-    }
     return container;
 }
-/**
- <m-option-base class="m-option-single-answer">
-    <input type="radio" id="radio1" name="radios" />
-    <label for="radio1">
-        <span class="a-icon-multistate" data-icon-type="single"></span>
-        <span class="a-label-option">
-            Radio label
-        </span>
-    </label>
-</m-option-base>
- */
