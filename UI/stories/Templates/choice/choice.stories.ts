a62e4c52145728721dccb6af1f104795e2b3de89
CreativeStrategyGuy <kevin.gray@ipsos.com>
2025-05-08 16:57:22 +0100

    Storybook: sublist stories

--- UI/stories/Templates/choice/choice.stories.ts
+++ UI/stories/Templates/choice/choice.stories.ts
@@ -108,10 +108,44 @@ export const TSimplePlusExclusive: StoryObj<typeof TChoice.TChoice_Story> = {
             const { args } = context; // Extract args from context
 
             try {
+                const xmlResponse = await fetch(
+                    './build/static/Dimensions/choice - simple with exclusive.xml',
+                );
+                const xslResponse = await fetch(
+                    './build/static/Dimensions/question.xsl',
+                );
+
+                if (!xmlResponse.ok || !xslResponse.ok) {
+                    throw new Error('Failed to fetch XML or XSLT files.');
+                }
+
+                const xmlData = await xmlResponse.text();
+                const xslData = await xslResponse.text();
+
+                return { xmlData, xslData };
+            } catch (error) {
+                console.error('Error in text loader:', error);
+                throw error;
+            }
+        },
+    ],
+    args: {
+        optionType: 'multi-answer',
+    },
+    render: (args, { loaded }) => TChoice.TChoice_Story(args, loaded),
+};
+TSimplePlusExclusive.storyName = 'A simple multi-answer with one exclusive';
+
+export const TSimpleSublist: StoryObj<typeof TChoice.TChoice_Story> = {
+    loaders: [
+        async (context) => {
+            const { args } = context; // Extract args from context
+
+            try {
                 const xmlFileName =
                     args.optionType === 'multi-answer'
-                        ? 'choice - simple - multi.xml'
-                        : 'choice - simple - single.xml';
+                        ? 'choice - simple sublist - multi.xml'
+                        : 'choice - simple sublist - single.xml';
 
                 const xmlResponse = await fetch(
                     `./build/static/Dimensions/${xmlFileName}`,
@@ -139,4 +173,3 @@ export const TSimplePlusExclusive: StoryObj<typeof TChoice.TChoice_Story> = {
     },
     render: (args, { loaded }) => TChoice.TChoice_Story(args, loaded),
 };
-TSimplePlusExclusive.storyName = 'A simple multi-answer with one exclusive';
