e972c4386dcd199f3dbde818233b89a92135c348
CreativeStrategyGuy <kevin.gray@ipsos.com>
2025-05-08 16:28:06 +0100

    Storybook: minor edits

--- UI/stories/Templates/choice/choice.stories.ts
+++ UI/stories/Templates/choice/choice.stories.ts
@@ -100,7 +100,7 @@ export const TSimplelist: StoryObj<typeof TChoice.TChoice_Story> = {
     },
     render: (args, { loaded }) => TChoice.TChoice_Story(args, loaded),
 };
-ASimpleList.storyName = 'A simple list with a heading';
+TSimpleList.storyName = 'A simple list with a heading';
 
 export const TSimplePlusExclusive: StoryObj<typeof TChoice.TChoice_Story> = {
     loaders: [
