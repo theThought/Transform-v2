6622ed2e4225af12014abff775b4bd7303563871
Richard Abrams <richard@purpleplanet.com>
2025-05-13 23:59:59 +0100

    Bring slider labels into the o-slider component

--- UI/src/javascript/web-components/o-slider.ts
+++ UI/src/javascript/web-components/o-slider.ts
@@ -2,6 +2,10 @@ import Component from './component';
 
 export default class OSlider extends Component {
     private element: HTMLInputElement | null = null;
+    private range: HTMLInputElement | null = null;
+    private min = 0;
+    private max = 100;
+    private step = 10;
 
     constructor() {
         super();
@@ -23,13 +27,51 @@ export default class OSlider extends Component {
         this.element.value = e.detail.element.value;
     }
 
+    private tickLabels(): void {
+        if (
+            !this.properties.hasOwnProperty('ticklabels') ||
+            typeof this.properties.ticklabels !== 'number'
+        ) {
+            return;
+        }
+
+        const labelsElement = this.querySelector('.m-label-marks');
+        if (!labelsElement) return;
+
+        let step = isNaN(this.properties.ticklabels)
+            ? 10
+            : this.properties.ticklabels;
+
+        if (step === 0) {
+            step = Math.floor(((this.max - this.min) / 100) * 10);
+        }
+
+        for (let i = this.min; i <= this.max; i = i + step) {
+            labelsElement.innerHTML =
+                labelsElement.innerHTML + '<span>' + i + '</span>';
+        }
+    }
+
+    private setProperties(): void {
+        if (!this.range) return;
+
+        this.min = this.range.min ? Number(this.range.min) : this.min;
+        this.max = this.range.max ? Number(this.range.max) : this.max;
+        this.step = this.properties.step
+            ? Number(this.properties.step)
+            : this.step;
+    }
+
     private init(): void {
         this.addEventListener('broadcastChange', this);
+        this.setProperties();
+        this.tickLabels();
     }
 
     public connectedCallback(): void {
         super.connectedCallback();
         this.element = this.querySelector('input[type="hidden"]');
+        this.range = this.querySelector('input[type="range"]');
         this.init();
     }
 }
