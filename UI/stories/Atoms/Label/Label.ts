bd61a87074a8a49e5c1f6c93bf178dec008d4346
Richard Abrams <richabramsuk@outlook.com>
2025-05-16 14:51:13 +0100

    Make the label text parameter optional
    So that the code can be reused where the web component is setting the label text directly but may not have set a label the label text is now optional. Label text is only used by Storybook to provide examples.

--- UI/stories/Atoms/Label/Label.ts
+++ UI/stories/Atoms/Label/Label.ts
@@ -1,40 +1,37 @@
 // label.ts
-export function ALabelPre(theLabel:string): HTMLSpanElement {
+export function ALabelPre(theLabel?: string): HTMLSpanElement {
     const container: HTMLSpanElement = document.createElement('span');
     container.textContent = theLabel;
     container.classList.add('a-label-pre');
     return container;
 }
 
-export function ALabelPost(theLabel:string): HTMLSpanElement {
+export function ALabelPost(theLabel?: string): HTMLSpanElement {
     const container: HTMLSpanElement = document.createElement('span');
     container.textContent = theLabel;
     container.classList.add('a-label-post');
     return container;
-};
+}
 
-export function ALabelQuestion(theLabel:string): HTMLLabelElement {
+export function ALabelQuestion(theLabel: string): HTMLLabelElement {
     const container: HTMLLabelElement = document.createElement('label');
     container.setAttribute('for', 'q-text');
     container.setAttribute('id', 'q-label');
     container.classList.add('a-label-question');
-
     container.textContent = theLabel;
     return container;
 }
 
-export function ALabelOption(theLabel:string): HTMLLabelElement {
+export function ALabelOption(theLabel: string): HTMLLabelElement {
     const container: HTMLLabelElement = document.createElement('label');
     container.classList.add('a-label-option');
-
     container.textContent = theLabel;
     return container;
 }
 
-export function ALabelHeadingSublist(theLabel:string): HTMLDivElement {
+export function ALabelHeadingSublist(theLabel: string): HTMLDivElement {
     const container: HTMLDivElement = document.createElement('div');
     container.classList.add('a-label-sublist');
-
     container.textContent = theLabel;
     return container;
 }
