3921f24f920b1d9888c71b9e927eb41a901e443a
Richard Abrams <richabramsuk@outlook.com>
2025-05-29 00:21:19 +0100

    Remove tab submission check

--- UI/src/javascript/web-components/m-option-tab.ts
+++ UI/src/javascript/web-components/m-option-tab.ts
@@ -2,11 +2,9 @@ import MOptionBase from './m-option-base';
 
 export default class MOptionTab extends MOptionBase {
     protected onClick(e: Event): void {
-        if (this.dataset.checked !== 'true') {
-            const form = this.closest('form');
-            if (form) form.submit();
-        }
-
         super.onClick(e);
+
+        const form = this.closest('form');
+        if (form) form.submit();
     }
 }
