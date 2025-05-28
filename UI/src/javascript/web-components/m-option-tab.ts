53bea171d6bf97935577e5a470c1d01b1513db22
Richard Abrams <richabramsuk@outlook.com>
2025-05-29 00:26:50 +0100

    Fix tab submission

--- UI/src/javascript/web-components/m-option-tab.ts
+++ UI/src/javascript/web-components/m-option-tab.ts
@@ -2,9 +2,10 @@ import MOptionBase from './m-option-base';
 
 export default class MOptionTab extends MOptionBase {
     protected onClick(e: Event): void {
-        super.onClick(e);
-
-        const form = this.closest('form');
-        if (form) form.submit();
+        if (this.dataset.checked !== 'true') {
+            super.onClick(e);
+            const form = this.closest('form');
+            if (form) form.submit();
+        }
     }
 }
