9d87b4f73ad7fa637775dcaf7c673bc819b6db30
CreativeStrategyGuy <kevin.gray@ipsos.com>
2025-06-02 18:29:41 +0100

    Storybook: removed static id

--- UI/stories/Atoms/List/option.ts
+++ UI/stories/Atoms/List/option.ts
@@ -2,7 +2,7 @@ export function AListOption_Story(args: any): HTMLElement {
     const container: HTMLLIElement = document.createElement('li');
     container.setAttribute('class', 'a-list-option');
     container.setAttribute('data-value', args.dataValue);
-    container.setAttribute('id', `args.id`);
+    container.setAttribute('id', args.id);
     container.textContent = args.content;
     return container;
 }
