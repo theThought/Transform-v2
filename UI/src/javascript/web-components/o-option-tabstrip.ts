5886508cb0c1793c2cee095f9ca1cc3c116123df
Richard Abrams <richabramsuk@outlook.com>
2025-06-21 01:03:37 +0100

    Initial work on tabstrip error handling

--- UI/src/javascript/web-components/o-option-tabstrip.ts
+++ UI/src/javascript/web-components/o-option-tabstrip.ts
@@ -5,4 +5,24 @@ export default class OOptionTabstrip extends OOptionSublist implements Subject {
     constructor() {
         super();
     }
+
+    private checkCurrentSelection(): void {
+        const currentTabSelection: HTMLInputElement | null = this.querySelector(
+            'm-option-tab[data-checked="true"] input',
+        );
+
+        const currentReportedPage = '';
+
+        if (!currentTabSelection || !currentReportedPage) return;
+
+        const currentTabValue = currentTabSelection.value;
+
+        if (currentTabValue !== currentReportedPage) {
+        }
+    }
+
+    public connectedCallback(): void {
+        super.connectedCallback();
+        this.checkCurrentSelection();
+    }
 }
