db0965bd8dcf3e57703ffab14bdd71f9dd76bf0c
CreativeStrategyGuy <kevin.gray@ipsos.com>
2025-06-25 12:26:51 +0100

    Storybook, first attempt tList

---
+++ UI/stories/Templates/List/list.stories.ts
@@ -0,0 +1,56 @@
+import { Meta, StoryObj } from '@storybook/web-components';
+
+import * as TList from './list';
+
+export default {
+    title: 'Templates/list',
+    component: 'transform-component',
+    parameters: {
+        status: { type: 'beta' },
+    },
+    argTypes: {
+        'properties.listsize': {
+            control: { type: 'number', min: 0 },
+            name: 'listsize',
+            description:
+                'Configures how many options should be displayed in the list.',
+            table: {
+                type: { summary: 'number', min: 0 },
+                category: 'Custom properties',
+                defaultValue: { summary: '6' },
+            },
+        },
+    },
+} as Meta;
+
+export const TListbox: StoryObj<typeof TList.TList_Story> = {
+    loaders: [
+        async (context) => {
+            const { args } = context; // Extract args from context
+
+            try {
+
+                const xmlResponse = await fetch(
+                    `./build/static/Dimensions/list - organisation.xml`,
+                );
+                const xslResponse = await fetch(
+                    './build/static/Dimensions/question.xsl',
+                );
+
+                if (!xmlResponse.ok || !xslResponse.ok) {
+                    throw new Error('Failed to fetch XML or XSLT files.');
+                }
+
+                const xmlData = await xmlResponse.text();
+                const xslData = await xslResponse.text();
+
+                return { xmlData, xslData };
+            } catch (error) {
+                console.error('Error in text loader:', error);
+                throw error;
+            }
+        },
+    ],
+    render: (args, { loaded }) => TList.TList_Story(args, loaded),
+};
+TListbox.storyName = 'A list box with a small number of items';
---
+++ UI/stories/Templates/List/list.ts
@@ -0,0 +1,43 @@
+import TransformUtils from '../../../utils/TransformUtils';
+
+export function TList_Story(
+    args: any,
+    loaded: { xmlData: string; xslData: string },
+): HTMLElement {
+    const { xmlData, xslData } = loaded;
+
+    if (!xmlData || !xslData) {
+        throw new Error('Missing required XML or XSLT data.');
+    }
+
+    // Parse the XML and update attributes based on args
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xmlData, 'application/xml');
+    const controlElements = xmlDoc.querySelectorAll(
+        'Questions > Question > Control',
+    );
+    const styleElements = xmlDoc.querySelectorAll(
+        'Questions > Question > Control > Style',
+    );
+    const questionStyleElements = xmlDoc.querySelectorAll(
+        'Questions > Question > Style',
+    );
+    const dataProperties: any = {
+        listsize: args.listsize || false,
+    };
+
+    // Create JSON for data-properties
+    const dataPropertiesString = JSON.stringify(dataProperties);
+
+    // Update the Color attribute in Question Style elements
+    questionStyleElements.forEach((styleElement) => {
+        styleElement.setAttribute('Color', dataPropertiesString);
+    });
+
+    const serializer = new XMLSerializer();
+    const updatedXmlData = serializer.serializeToString(xmlDoc);
+
+    const container = TransformUtils.transform(updatedXmlData, xslData);
+
+    return container;
+}
