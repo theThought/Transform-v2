e379e6d71fc0be9de1b90ac3a5e08682856e425c
Richard Abrams <richabramsuk@outlook.com>
2025-08-27 16:07:32 +0100

    Update current tab based on properties

--- UI/src/javascript/web-components/m-option-tab.ts
+++ UI/src/javascript/web-components/m-option-tab.ts
@@ -1,11 +1,26 @@
 import MOptionBase from './m-option-base';
 
 export default class MOptionTab extends MOptionBase {
+    static observedAttributes = ['data-checked'];
+
     protected onClick(e: Event): void {
         if (this.dataset.checked !== 'true') {
             super.onClick(e);
             const form = this.closest('form');
             if (form) form.submit();
         }
+    }
+
+    private handleCheckChange(): void {
+        const checkbox: HTMLInputElement | null = this.querySelector('input');
+        if (!checkbox) return;
+
+        if (this.dataset.checked === 'true') {
+            checkbox.checked = true;
+        }
+    }
+
+    public attributeChangedCallback(name: string): void {
+        if (name === 'data-checked') this.handleCheckChange();
     }
 }
--- UI/src/javascript/web-components/o-option-tabstrip.ts
+++ UI/src/javascript/web-components/o-option-tabstrip.ts
@@ -1,23 +1,53 @@
 import OOptionSublist from './o-option-sublist';
-import { Subject } from '../interfaces';
 
-export default class OOptionTabstrip extends OOptionSublist implements Subject {
+interface CustomProperties {
+    balance: {
+        state: boolean;
+        minwidth: string;
+    };
+    onesize: {
+        state: boolean;
+        maxwidth: string;
+    };
+    tabstrip?: {
+        question: string;
+        tab: string;
+    };
+}
+
+export default class OOptionTabstrip extends OOptionSublist {
+    public properties: CustomProperties = {
+        balance: {
+            state: false,
+            minwidth: '',
+        },
+        onesize: {
+            state: false,
+            maxwidth: '',
+        },
+    };
+
     constructor() {
         super();
     }
 
     private checkCurrentSelection(): void {
-        const currentTabSelection: HTMLInputElement | null = this.querySelector(
-            'm-option-tab[data-checked="true"] input',
+        if (!this.properties.tabstrip) return;
+
+        const currentTab: HTMLInputElement | null = this.querySelector(
+            'm-option-tab[data-checked="true"]',
         );
 
-        const currentReportedPage = '';
+        if (!currentTab) return;
 
-        if (!currentTabSelection || !currentReportedPage) return;
-
-        const currentTabValue = currentTabSelection.value;
-
-        if (currentTabValue !== currentReportedPage) {
+        if (currentTab.value !== this.properties.tabstrip.tab) {
+            const desiredTabInput: HTMLInputElement | null = this.querySelector(
+                `m-option-tab input[value='${this.properties.tabstrip.tab}']`,
+            );
+            const desiredTab = desiredTabInput?.parentElement;
+            if (!desiredTab) return;
+            currentTab.dataset.checked = 'false';
+            desiredTab.dataset.checked = 'true';
         }
     }
 
