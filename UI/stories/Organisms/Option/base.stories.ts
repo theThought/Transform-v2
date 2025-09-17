9c2f1a075d9bd80ed5a3de0f67c9f4810a41c6a1
Richard Abrams <richabramsuk@users.noreply.github.com>
2025-09-17 14:34:45 +0300

    o-option-base in Storybook
    A basic implementation to test styles.

---
+++ UI/stories/Organisms/Option/base.stories.ts
@@ -0,0 +1,130 @@
+import { Meta, StoryObj } from '@storybook/web-components';
+import { OOptionBase_Story } from './base';
+
+export default {
+    title: 'Organisms/Option',
+    component: 'o-option-base', // Use the tag name of the custom element
+    parameters: {
+        status: { type: 'beta' },
+    },
+    argTypes: {
+        optionType: {
+            control: 'select',
+            options: ['single-answer', 'multi-answer'],
+            description: 'Single or multi-answer question',
+            table: {
+                type: { summary: 'string' },
+                defaultValue: { summary: 'single-answer' },
+            },
+        },
+        optionStatus: {
+            control: 'select',
+            options: ['unselected', 'selected', 'readonly'],
+            description: 'Display state of the option',
+            table: {
+                type: { summary: 'string' },
+                defaultValue: { summary: 'unselected' },
+            },
+        },
+        exclusive: {
+            control: 'boolean',
+            description: 'Does selecting this option deselect all others?',
+            table: {
+                type: { summary: 'boolean' },
+                category: 'Dimensions',
+                order: 1,
+                defaultValue: { summary: 'false' },
+            },
+        },
+        icon: {
+            control: 'text',
+            description: 'icon for the option',
+            table: {
+                type: { summary: 'text' },
+                category: 'Dimensions',
+                order: 5,
+                defaultValue: { summary: 'false' },
+            },
+        },
+        optionLabel: {
+            control: 'text',
+            description: 'Label for the option',
+            table: {
+                type: { summary: 'string' },
+                category: 'Dimensions',
+                order: 4,
+                defaultValue: { summary: 'This is the option label' },
+            },
+        },
+        categoryId: {
+            control: 'text',
+            description: 'Unique identifier for the category',
+            table: {
+                type: { summary: 'string' },
+                category: 'Dimensions',
+                order: 3,
+                defaultValue: { summary: '_C0' },
+            },
+        },
+        questionId: {
+            control: 'text',
+            description: 'Unique identifier for the question',
+            table: {
+                type: { summary: 'string' },
+                category: 'Dimensions',
+                order: 2,
+                defaultValue: { summary: '_Q0' },
+            },
+        },
+        questionName: {
+            control: 'text',
+            description: 'Name of the question',
+            table: {
+                type: { summary: 'string' },
+                category: 'Dimensions',
+                order: 6,
+                defaultValue: { summary: 'Example' },
+            },
+        },
+    },
+} as Meta;
+
+type SingleAnswer = StoryObj<typeof OOptionBase_Story>;
+export const StandardRadio: SingleAnswer = {
+    parameters: {
+        controls: {
+            include: ['optionLabel', 'exclusive'],
+        },
+    },
+    args: {
+        optionType: 'single-answer',
+        exclusive: false,
+        questionId: '_Q0',
+        categoryId: '_C0',
+        questionName: 'RadioExample',
+        optionLabel: 'This is standard single-choice option',
+        iconType: 'radio',
+    },
+    render: (args) => OOptionBase_Story(args),
+};
+StandardRadio.storyName = 'Exclusive option (single-choice)';
+
+type MultiAnswer = StoryObj<typeof OOptionBase_Story>;
+export const StandardCheck: MultiAnswer = {
+    parameters: {
+        controls: {
+            include: ['optionLabel', 'exclusive'],
+        },
+    },
+    args: {
+        optionType: 'multi-answer',
+        exclusive: false,
+        questionId: '_Q0',
+        categoryId: '_C1',
+        questionName: 'CheckExample',
+        optionLabel: 'This is standard multi-choice option',
+        iconType: 'checkbox',
+    },
+    render: (args) => OOptionBase_Story(args),
+};
+StandardCheck.storyName = 'Non-exclusive option (multi-choice)';
---
+++ UI/stories/Organisms/Option/base.ts
@@ -0,0 +1,39 @@
+export function OOptionBase_Story(args: any): HTMLElement {
+    const container: HTMLElement = document.createElement('o-option-base');
+    const containerClass: string = 'o-option-' + args.optionType;
+    const inputType: string =
+        args.optionType === 'single-answer' ? 'radio' : 'checkbox';
+    const customProperties = args.properties
+        ? JSON.stringify(args.properties)
+        : '';
+
+    container.setAttribute('data-exclusive', args.exclusive);
+    container.setAttribute('data-question-id', args.questionId);
+    container.setAttribute('data-question-group', args.questionName);
+    container.setAttribute('class', containerClass + ' below');
+    container.setAttribute('data-hidden', 'false');
+    container.setAttribute('data-properties', customProperties);
+
+    const inputElement: HTMLInputElement = document.createElement('input');
+    inputElement.setAttribute('type', inputType);
+    inputElement.setAttribute('id', args.questionId + args.categoryId);
+    inputElement.setAttribute('name', args.questionName);
+    inputElement.setAttribute('data-question-group', args.questionName);
+    container.appendChild(inputElement);
+
+    const labelElement = document.createElement('label');
+    labelElement.setAttribute('for', args.questionId + args.categoryId);
+
+    const iconElement = document.createElement('span');
+    iconElement.setAttribute('class', 'a-icon-multistate');
+    iconElement.setAttribute('data-icon-type', args.iconType);
+    labelElement.appendChild(iconElement);
+
+    const textElement = document.createElement('span');
+    textElement.setAttribute('class', 'a-label-option');
+    textElement.textContent = args.optionLabel;
+    labelElement.appendChild(textElement);
+    container.appendChild(labelElement);
+
+    return container;
+}
