d070c109c0a5e184cea95eea0bae95a2e7a83446
Richard Abrams <richabramsuk@outlook.com>
2025-10-02 21:59:00 +0100

    Allows tab focus on stand-alone lists
    This does introduce some oddity with the tab sequence which I haven't yet zeroed in on. I believe it's to do with focus for scrolling vs moving the current selection but is better than not allowing keyboard focus at all.

--- UI/src/javascript/web-components/o-list.ts
+++ UI/src/javascript/web-components/o-list.ts
@@ -573,10 +573,6 @@ export default class OList extends Component implements Observer {
         this.tabIndex = -1;
     }
 
-    private removeLocalTabIndex(): void {
-        this.querySelector('ul')?.setAttribute('tabindex', '-1');
-    }
-
     public connectedCallback(): void {
         super.connectedCallback();
 
@@ -601,8 +597,6 @@ export default class OList extends Component implements Observer {
         if (this.control) {
             this.control.addObserver(this);
             this.removeTabIndex();
-        } else {
-            this.removeLocalTabIndex();
         }
     }
 }
